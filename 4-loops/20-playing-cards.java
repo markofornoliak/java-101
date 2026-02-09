6181596584301380c509e4bcaa49d9564438c073
markofornoliak <130382040+markofornoliak@users.noreply.github.com>
2026-02-09 19:16:41 +0100

    Else statement isnt necessry here

--- 4-loops/20-playing-cards.java
+++ 4-loops/20-playing-cards.java
@@ -1,13 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        // Write your code here 💖
-        for (int i = 1; i <= 52; i++) {
-            if (i == 32) {
-                System.out.println("An incorrect card was found in item " + i);
+        for(int i = 1; i <= 52; i++){
+            System.out.println("Card "+i);
+            if(i == 32){
+                System.out.println("An incorrect card was found in item "+i+".");
                 break;
-            } else {
-                System.out.println("Card " + i);
             }
         }
+        
     }
 }
+
